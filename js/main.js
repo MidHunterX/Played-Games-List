e8800b57236c40eae34eb14aeeac148d4eccc69c
Midhun Jinan <43461693+MidHunterX@users.noreply.github.com>
2021-12-31 15:16:34 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 \*-------------------------------*/
 
 function game_cards() {
+
     // Fetch API for requesting resources
     fetch("./js/db.json")
         .then(function (response) {
@@ -11,38 +12,23 @@ function game_cards() {
 
         .then(function (data) {
             // Fruit Loop [i]
+            n = data.length
+
             for (var i = 0; i < data.length; i++) {
 
                 document.getElementById("game-list").innerHTML +=
                     "<div class='card-bg'>" +
-                    "<h3>" + data[i].id + "</h3>" +
+                    "<p class='text-serial'>" + n + "</p>" +
                     "<img class='cover' src='img/" + data[i].img + ".jpg'>" +
                     "<div class='card-padding'>" +
-                    "<h2>" + data[i].game + "</h2>" +
-                    "<p>" + data[i].year + "</p>" +
+                    "<h3 class='text-title'>" + data[i].game + "</h3>" +
+                    "<p class='text-year'>" + data[i].year + "</p>" +
                     "</div></div>";
+                n--;
             }
         })
 
         .catch(function (err) {
             console.log(err);
         });
-
 }
-
-/* Old Code :(
-function game_cards(data) {
-    fetch("./js/db.json")
-    var table = document.getElementById('game-list')
-    for (let i = 0; i < data.length; i++) {
-        var string = "<div class='card-bg'>";
-        string += "<h3>" + value.id + "</h3>";
-        string += "<img class='cover' src='img/" + value.img + ".jpg'>";
-        string += "<div class='card-padding'>";
-        string += "<h2>" + value.game + "</h2>";
-        string += "<p>" + value.year + "</p>";
-        string += "</div></div>";
-    }
-    table.innerHTML += string;
-}
-*/
