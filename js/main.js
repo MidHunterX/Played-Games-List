bd2f8e65184c35f305a4a8b6afa29a33156554a4
MidHunterX <midhunterx@gmail.com>
2024-07-28 15:22:22 +0530

    🚚 Feat: Ability to Sort Game Cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+let gameData = [];
+
 function fetchGameData() {
   return fetch("data.json")
-    .then(response => response.json())
-    .catch(err => {
+    .then((response) => response.json())
+    .catch((err) => {
       console.error("Error fetching game data:", err);
       return [];
     });
@@ -23,9 +25,40 @@ function renderGameCards(data) {
   document.getElementById("game-list").innerHTML = html;
 }
 
+function sort_game_cards() {
+  const sortOption = document.getElementById("sort-options").value;
+  let sortedData = [...gameData];
+  // The triple dots are known as the spread operator, which takes an
+  // iterable(array, string, or object) and expands it to individual values.
+  switch (sortOption) {
+    case "id-asc":
+      sortedData.sort((a, b) => a.id - b.id);
+      break;
+    case "id-desc":
+      sortedData.sort((a, b) => b.id - a.id);
+      break;
+    case "year-asc":
+      sortedData.sort((a, b) => a.year - b.year);
+      break;
+    case "year-desc":
+      sortedData.sort((a, b) => b.year - a.year);
+      break;
+    case "name-asc":
+      sortedData.sort((a, b) => a.game.localeCompare(b.game));
+      break;
+    case "name-desc":
+      sortedData.sort((a, b) => b.game.localeCompare(a.game));
+      break;
+  }
+  renderGameCards(sortedData);
+}
+
 function game_cards() {
   fetchGameData()
     /* .then(function(data) { return renderGameCards(data); }) */
-    .then(data => renderGameCards(data))
-    .catch(err => console.error("Error rendering game cards:", err));
+    .then((data) => {
+      gameData = data;
+      renderGameCards(gameData);
+    })
+    .catch((err) => console.error("Error rendering game cards:", err));
 }
